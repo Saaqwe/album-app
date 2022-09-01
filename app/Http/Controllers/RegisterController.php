fa273682bb9a624d241056b2dfce0418a8a5c021
Timur <timurahaha@gmail.com>
2022-09-01 22:57:14 +0400

    added method for api request in RegisterController

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -9,8 +9,49 @@ use Illuminate\Support\Facades\Auth;
 class RegisterController extends Controller
 {
     public function save(Request $request) {
+        if($request->expectsJson()) {
+            return self::saveIfJsonExpected($request);
+        } else {
+            if(Auth::check()) {
+                return redirect('user.private');
+            }
+            $validateFields = $request->validate([
+                'name' => 'required',
+                'login' => 'required|unique:users',
+                'email' => 'required|unique:users',
+                'password' => 'required'
+            ]);
+
+            if(User::where('email', $validateFields['email'])->exists()) {
+                redirect(route('user.registration'))->withErrors([
+                    "email" => 'Пользователь с таким email уже зарегестрирован'
+                ]);
+            }
+
+            if(User::where('email', $validateFields['login'])->exists()) {
+                redirect(route('user.registration'))->withErrors([
+                    "email" => 'Пользователь с таким login уже зарегестрирован'
+                ]);
+            }
+
+
+            $user = User::create($validateFields);
+            if($user) {
+                $user->createToken("auth_token");
+                Auth::login($user);
+                return redirect()->to(route('user.private'));
+            }
+
+            return redirect(route('user.login'))->withErrors([
+                'formError' => "Error during creating user"
+            ]);
+        }
+
+    }
+
+    public function saveIfJsonExpected (Request $request) {
         if(Auth::check()) {
-            return redirect('user.private');
+            return response()->json();
         }
         $validateFields = $request->validate([
             'name' => 'required',
@@ -18,33 +59,26 @@ class RegisterController extends Controller
             'email' => 'required|unique:users',
             'password' => 'required'
         ]);
-
         if(User::where('email', $validateFields['email'])->exists()) {
-            redirect(route('user.registration'))->withErrors([
-               "email" => 'Пользователь с таким email уже зарегестрирован'
-            ]);
+            return response()->json([
+                'errors' => "User with this email already exist",
+            ], 422);
         }
-
-        if(User::where('email', $validateFields['login'])->exists()) {
-            redirect(route('user.registration'))->withErrors([
-                "email" => 'Пользователь с таким email уже зарегестрирован'
-            ]);
+        if(User::where('login', $validateFields['login'])->exists()) {
+            return response()->json([
+                'errors' => "User with this login already exist",
+            ], 422);
         }
-
-
         $user = User::create($validateFields);
         if($user) {
             $user->createToken("auth_token");
             Auth::login($user);
-            return redirect()->to(route('user.private'));
+            return response()->json();
         }
 
-        return redirect(route('user.login'))->withErrors([
-            'formError' => "Error during creating user"
-        ]);
-    }
-
-    public function saveIfJsonExpected () {
+        return response()->json([
+            'errors' => "Some validation errors",
+        ], 422);
 
     }
 }
