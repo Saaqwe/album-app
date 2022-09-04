a147598fca65f0386a013f04752b98a7d7b7ed94
Timur <timurahaha@gmail.com>
2022-09-04 23:59:38 +0400

    Article model and table added

---
+++ database/migrations/2022_09_04_131344_create_articles_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->id()->unique();
+            $table->string('title', 255);
+            $table->string('image_name')->nullable()->unique();
+            $table->string('main_text');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('articles');
+    }
+};
